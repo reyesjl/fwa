a1c9aad7bfe67ec04670453581ce840e4af3db2c
reyesjl <reyesjl.nine@hotmail.com>
2024-01-04 12:00:48 -0500

    add views for new pages;

--- f5index/views.py
+++ f5index/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'f5index/index.html')
+
+def about(request):
+    return render(request, 'f5index/about.html')
+
+def store(request):
+    return render(request, 'f5index/store.html')
+
+def services(request):
+    return render(request, 'f5index/services.html')
