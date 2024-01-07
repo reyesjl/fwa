c08b8ade368bce708001264ac1e94d75580ef170
reyesjl <reyesjl.nine@hotmail.com>
2024-01-07 17:24:07 -0500

    create recruiting PlayerEntry model for hanlding applicants;

--- recruiting/models.py
+++ recruiting/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class PlayerEntry(models.Model):
+    COUNTRY_CHOICES = [
+        ('USA', 'United States of America'),
+        ('IRE', 'Ireland'),
+        ('SCT', 'Scotland'),
+        ('ENG', 'England'),
+        # Add more country choices as needed
+    ]
+
+    name = models.CharField(max_length=100)
+    age = models.IntegerField(validators=[MinValueValidator(14)])
+    country_of_interest = models.CharField(max_length=3, choices=COUNTRY_CHOICES)
+    school_of_interest = models.CharField(max_length=100)
+    rugby_history = models.TextField()
+    cover_letter = models.TextField()
+    country_of_residence = models.CharField(max_length=100)
+    contact_email = models.EmailField()
+    contact_phone = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return f"{self.name} - {self.country_of_residence}"
