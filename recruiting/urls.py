4ec8a9484e904a2950eb15a2d3a33f196746e509
reyesjl <reyesjl.nine@hotmail.com>
2024-01-07 17:58:45 -0500

    create the url path to access PlayerEntryForm;

--- recruiting/urls.py
+++ recruiting/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'recruiting'
 urlpatterns = [
     path('services/recruiting/', views.home, name='home'),
+    path('services/recruiting/apply', views.player_entry_view, name='apply'),
 ]
