8d9d5ad01b4c891d236a2d6255cd06ecc2f89763
reyesjl <reyesjl.nine@hotmail.com>
2024-01-07 18:22:35 -0500

    add the player entry view for the form; add success view url path;

--- recruiting/views.py
+++ recruiting/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PlayerEntryForm
 
 def home(request):
     return render(request,'recruiting/home.html')
+
+def player_entry_view(request):
+    if request.method == 'POST':
+        form = PlayerEntryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Redirect the user to the success page
+            return redirect('recruiting:apply_success')
+    else:
+        form = PlayerEntryForm()
+    
+    return render(request, 'recruiting/player_entry_view.html', {'form': form})
+
+def success(request):
+    return render(request, 'recruiting/success.html')
