7549719b1a0838a707319c8f31913f91b5c71416
reyesjl <reyesjl.nine@hotmail.com>
2024-01-07 19:33:19 -0500

    edit school of interest to pull from schools;

--- recruiting/forms.py
+++ recruiting/forms.py
@@ -1,7 +1,13 @@
 from django import forms
-from .models import PlayerEntry
+from .models import PlayerEntry, School
 
 class PlayerEntryForm(forms.ModelForm):
+    school_of_interest = forms.ModelChoiceField(queryset=School.objects.all())
+
     class Meta:
         model = PlayerEntry
-        exclude = ['status'] # This is for staff usage only
+        exclude = ['status']  # This is for staff usage only
+
+    def __init__(self, *args, **kwargs):
+        super(PlayerEntryForm, self).__init__(*args, **kwargs)
+
