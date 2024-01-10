e8dcfe3281ca03234f05e221b38914cb808cd417
reyesjl <reyesjl.nine@hotmail.com>
2024-01-09 22:24:05 -0500

    add view path for product_detail;

--- product/views.py
+++ product/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def store(request):
     products = Product.objects.all()
     return render(request, 'product/store.html', {'products': products})
 
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'product/product_detail.html', {'product': product})
