3871e734929e81df0d0bfac7388337a41a62d7b1
reyesjl <reyesjl.nine@hotmail.com>
2024-01-11 13:44:06 -0500

    add purchase and success url paths;

--- product/urls.py
+++ product/urls.py
@@ -5,4 +5,6 @@ app_name = 'product'
 urlpatterns = [
     path('store/products/', views.store, name='store'),
     path('store/products/<int:product_id>', views.product_detail, name='product_detail'),
+    path('store/purchase/<int:product_id>', views.purchase, name='purchase'),
+    path('store/purchase/success', views.success, name='payment_success'),
 ]
