f4556ce63560030481400c45a95697898accc86b
reyesjl <reyesjl.nine@hotmail.com>
2024-01-15 10:45:13 -0500

    add school detail route;

--- recruiting/urls.py
+++ recruiting/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('services/recruiting/', views.home, name='home'),
     path('services/recruiting/apply', views.player_entry_view, name='apply'),
     path('services/recruiting/success', views.success, name='apply_success'),
+    path('services/recruiting/schools/<int:school_id>', views.school_detail, name='school_detail'),
 ]
