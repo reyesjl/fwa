75e6fb71e8f7ed0a7c90ce58ac292c8ce4613548
Jose <reyesjl.nine@hotmail.com>
2024-02-03 00:08:21 +0000

    update settings to use env defined database; postgresql in production;

--- firstfive/settings.py
+++ firstfive/settings.py
@@ -66,10 +66,16 @@ WSGI_APPLICATION = 'firstfive.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': config('DB_ENGINE', default='django.db.backends.sqlite3'),
+        'NAME': config('DB_NAME', default=BASE_DIR / "db.sqlite3"),
+        'USER': config('DB_USER', default=''),
+        'PASSWORD': config('DB_PASSWORD', default=''),
+        'HOST': config('DB_HOST', default=''),
+        'PORT': config('DB_PORT', default=''),
     }
 }
+
+
 
 
 # Password validation
