41b4278678aecaf40179c742e891d9980376818f
Jose <reyesjl.nine@hotmail.com>
2024-02-13 20:30:13 +0000

    copy product templates into store/products; remove old models from admin; add product models to store admin page;

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,2 @@
 from django.contrib import admin
-from .models import Product, ProductDetail, ProductImage
-
-admin.site.register(Product)
-admin.site.register(ProductDetail)
-admin.site.register(ProductImage)
 
